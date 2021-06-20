a3567df1bc23e2e5e046c151e6d09ea0c33002a1
engineersunit <engineersunit@gmail.com>
2021-06-20 08:17:06 +0530

    another commit

--- DIVSUM.java
+++ DIVSUM.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class DIVSUM {
+public class DIVSUM  {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        int numOfTestCases = sc.nextInt();
